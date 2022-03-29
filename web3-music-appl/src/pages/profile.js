68f06b0b1a64eb521bafb3a7dfcaeb34fc5301c3
Srujan Vepuri <Svepuri@albany.edu>
2022-03-28 21:38:33 -0400

    not commit

--- web3-music-appl/src/pages/profile.js
+++ web3-music-appl/src/pages/profile.js
@@ -2,7 +2,26 @@ import React from "react";
 import styles from '../App.css'
 import WallectCard from "../wallectcard";
 
+// async function onInit() {
+//     await window.ethereum.enable();
+//     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//     const account = accounts[0];
+//     console.log(account)
+//      window.ethereum.on('accountsChanged', function (accounts) {
+//         // Time to reload your interface with accounts[0]!
+//         console.log(accounts[0])
+//         return account
+//        });
+// }
+
+// let some =await onInit();
+// console.log(some+" asdasdasdassdsa")
+
 function userProfile(){
+    
+    
+
+
     return (
         <body style={{backgroundColor:"bisque"}}>
             <div class="main">
