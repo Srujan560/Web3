03094d43e291f538922cba9788b16df836290f76
evanphilipps <Oliver123>
2022-03-28 20:18:14 -0400

    fixed log out button to match log in button

--- web3-music-appl/src/pages/profile.js
+++ web3-music-appl/src/pages/profile.js
@@ -2,24 +2,13 @@ import React from "react";
 import styles from '../App.css';
 import WallectCard from "../wallectcard";
 import user from "../App";
-import {Helmet} from 'react-helmet';
-var userID, userBalance;
 
-function getUserData(){
-    document.getElementById("addr").innerHTML = "This is an address";
-}
 
 function userProfile(){
     return (
         <body style={{backgroundColor:"bisque"}}>
             <div class="main">
-                <Helmet>
-                    <script>
-                        var address = document.getElementById("wallet");
-                        address.innerHTML = "0x03C5bFbCE62Dea655A410ffB368984e1cfd43019";
-                        console.log(address)
-                    </script>
-                </Helmet>
+                
                 <center>
                     <h1>Profile</h1>
                     <div class="information">
