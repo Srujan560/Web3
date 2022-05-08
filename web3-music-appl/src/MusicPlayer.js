e4fcb48a192cf1776a9be5c7459065c9f81510c8
Jade Lathbridge <jade-lathbridge@Jades-MacBook-Pro.local>
2022-05-07 23:34:44 -0400

    added queue functionality to music player and made some css improvements

--- web3-music-appl/src/MusicPlayer.js
+++ web3-music-appl/src/MusicPlayer.js
@@ -13,19 +13,24 @@ import "react-h5-audio-player/lib/styles.css";
 //import "instantsearch.css/themes/satellite.css";
 
 function MusicPlayer() {
-  //   const appId = "latency";
-  //   const apiKey = "6be0576ff61c053d5f9a3225e2a90f76";
   const appId = "MVJ73ZN1LB";
   const apiKey = "676191f4468f9a895db992a7241163cc";
   const searchClient = algoliasearch(appId, apiKey);
 
-  const [songQueue, setsongQueue] = useState([]);
+  const [songQueue, setSongQueue] = useState([]);
   const [playNow, setplayNow] = useState("");
 
   const Hit = ({ hit }) => {
-    const handleClick = () => {
+    const handlePlay = () => {
       setplayNow(hit.audio);
       //alert("Now Playing: " + hit.song + "\nBy: " + hit.artist);
+    };
+    const handleQueue = () => {
+      const queuedSongObj = {
+        audio: hit.audio,
+        song: hit.song
+      };
+      setSongQueue((oldQueue) => [...oldQueue, queuedSongObj]);
     };
     return (
       <div className={styles.hit}>
@@ -34,12 +39,17 @@ function MusicPlayer() {
             <img className={styles.coverImg} src={hit.image} width="80%" />
             <img
               className={styles.hoverPlay}
-              onClick={handleClick}
+              onClick={handlePlay}
               src="http://www.slatecube.com/images/play-btn.png"
             />
           </center>
-          <h4 class="song">{hit.song}</h4>
-          <h3 class="artist">{hit.artist}</h3>
+          <div className="songInfo">
+            <h4 class="song">{hit.song}</h4>
+            <h3 class="artist">{hit.artist}</h3>
+            <button className={styles.queueButton} onClick={handleQueue}>
+              Add to Queue
+            </button>
+          </div>
         </div>
       </div>
     );
@@ -52,34 +62,60 @@ function MusicPlayer() {
     );
   };
 
-  //replace seach bar in nav bar with autocomplete bar!!
+  const handleRemoveSong = (e) => {
+    console.log("trying to remove");
+    const song = e.target.getAttribute("removeSongs");
+    setSongQueue(songQueue.filter((items) => items.song !== song));
+  };
+  const handlePlayNext = () => {
+    setplayNow(songQueue.shift().audio);
+    setSongQueue(songQueue);
+  };
+
   return (
-    <div className={styles.musicPlayerPage} style={{}}>
+    <div className={styles.musicPlayerPage}>
+      {/* <SongList /> */}
       <React.Fragment>
         {/* music search functionality */}
-        <div className={styles.searchcont}>
-          <InstantSearch
-            searchClient={searchClient}
-            indexName="test_MusicUploads"
-          >
-            <SearchBox
-              className={styles.searchBox}
-              translations={{ placeholder: "Search for Music" }}
-            />
-            <main>
-              <Content />
-            </main>
-          </InstantSearch>
+        <div className={styles.contentRow}>
+          <div className={styles.searchcont}>
+            <InstantSearch
+              searchClient={searchClient}
+              indexName="test_MusicUploads"
+            >
+              <center>
+                <SearchBox
+                  className={styles.searchBox}
+                  translations={{ placeholder: "Search for Music" }}
+                />
+              </center>
+              <main>
+                <Content />
+              </main>
+            </InstantSearch>
+          </div>
+          <div className={styles.queueContainer}>
+            <center className={styles.queueTitle}>Queue</center>
+            {songQueue.map(function (songs, index) {
+              return (
+                <div className={styles.queuedSong} key={index}>
+                  {songs.song}
+                  <button removeSongs={songs.song} onClick={handleRemoveSong}>
+                    X
+                  </button>
+                </div>
+              );
+            })}
+          </div>
         </div>
-        {/* <Player /> */}
         <AudioPlayer
           className={styles.audioPlayer}
           //autoPlay
           src={playNow}
           onPlay={(e) => console.log("onPlay")}
+          onEnded={handlePlayNext}
           // other props here
         />
-        {/* <SongList /> */}
       </React.Fragment>
     </div>
   );
